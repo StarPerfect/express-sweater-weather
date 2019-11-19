4e290d893fb9ac15bd1ffd2a674b61861ead2593
Corina Allen <StarPerfect@gmail.com>
2019-11-18 18:33:22 -0700

    Update Knexfile with Heroku Postgres url

--- knexfile.js
+++ knexfile.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: '',
+    connection: 'postgres://pixwyexapothmp:9954869918636a97be4306e7498fc42d43f39d588f62a7a93f0ab9886e011354@ec2-107-21-110-75.compute-1.amazonaws.com:5432/df1d39bfjq3mf1',
     migrations: {
       directory: './db/migrations'
     },
