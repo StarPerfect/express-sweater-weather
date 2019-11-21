61badf37067bffae35e59bed1f27d7750668bcca
Corina Allen <StarPerfect@gmail.com>
2019-11-20 21:15:12 -0700

    Create Fav controller to handle CRUDing favorite locations

--- controllers/favoritesController.js
+++ controllers/favoritesController.js
@@ -1,17 +1,27 @@
 const environment = process.env.NODE_ENV || 'development';
-const configuration = require('/../knexfile')[environment];
+const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
-var user = require('/../models/user');
+var user = require('../models/user');
+var fetch = require('node-fetch');
 
 const create = (request, response) => {
-    console.log(request)
     database('users').where('apiKey', request.body.api_key)
         .then((user) => {
             if (user) {
-
+                 return database('favorites').insert({location: request.body.location, userId: user.id})
+                     .then(() => {
+                         response.status(200).send({message: `${request.body.location} has been added to your favorites`});
+                     })
+                     .catch((error) => {
+                         response.status(500).json({error});
+                     })
             } else {
                 response.status(401).send({error: 'Unauthorized'})
             };
         })
-});
+};
+
+module.exports = {
+    create
+};
